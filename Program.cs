2f84092128300c4461a89c85aa38ea796533a376
Husnoro <Husnoro>
2020-03-26 20:12:09 +0500

    исправленная домашка!

--- Program.cs
+++ Program.cs
@@ -6,44 +6,44 @@ namespace domashka2403
     {
         static void Main(string[] args)
         {
-        // { //ex #2
-        //  int a = 0 , b = 0;
-        //  if (a != b){
-        //      a = a > b ? a : b;
-        //      b = a > b ? a : b;
-        //  } else {
-        //      a = 0;
-        //      b = 0;
-        //  }
-        //  Console.WriteLine($"{a},{b} ");
-        // }
-        // { //ex #3
-        // Console.Write("Enter number; ");
-        // int operand1 = int.Parse(Console.ReadLine());
-        // Console.Write("Enter another number: ");
-        // int operand2 = int.Parse(Console.ReadLine());
-        // Console.Write("Enter operation: ");
-        // string sign = Console.ReadLine();
-        // switch (sign) {
-        //     case "+":
-        //      Console.WriteLine(operand1 + operand2);
-        //     break;
-        //     case "-":
-        //      Console.WriteLine(operand1 - operand2);
-        //     break;
-        //     case "*":
-        //      Console.WriteLine(operand1 * operand2);
-        //     break;
-        //     case "/":             
-        //     if ( operand2 == 0){
-        //         Console.WriteLine("cannot be divided by zero!");              
-        //     } else {
-        //     Console.WriteLine(operand1 / operand2);
-        //     }
-        //     break;
-        // }
-        // Console.ReadKey();
-        // }
+        { //ex #2
+         int a = 0 , b = 0;
+         if (a != b){
+             a = a > b ? a : b;
+             b = a > b ? a : b;
+         } else {
+             a = 0;
+             b = 0;
+         }
+         Console.WriteLine($"{a},{b} ");
+        }
+        { //ex #3
+        Console.Write("Enter number; ");
+        int operand1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter another number: ");
+        int operand2 = int.Parse(Console.ReadLine());
+        Console.Write("Enter operation: ");
+        string sign = Console.ReadLine();
+        switch (sign) {
+            case "+":
+             Console.WriteLine(operand1 + operand2);
+            break;
+            case "-":
+             Console.WriteLine(operand1 - operand2);
+            break;
+            case "*":
+             Console.WriteLine(operand1 * operand2);
+            break;
+            case "/":             
+            if ( operand2 == 0){
+                Console.WriteLine("cannot be divided by zero!");              
+            } else {
+            Console.WriteLine(operand1 / operand2);
+            }
+            break;
+        }
+        Console.ReadKey();
+        }
         {
             Console.Write("Enter number from 0 to 100:");
             int number = int.Parse(Console.ReadLine());
@@ -55,14 +55,14 @@ namespace domashka2403
                 Console.WriteLine("This number is from the third gap");
             } if(number <= 100 && number >= 50) { 
                 Console.WriteLine("This number is from the fourth gap");
-            } if(number >= 100) { 
+            } if(number > 100) { 
                 Console.WriteLine("Eror");                
             } if(number <= -1) { 
                 Console.WriteLine("Eror");
             }
 
         }
-
+        
         }
     }
 }
